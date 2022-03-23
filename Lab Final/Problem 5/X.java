fc725b142ca4ed3e4decdafce8111cc92af40b5d
Jahidul Islam Rahat <jahidulislamrahat97@gmail.com>
2022-03-23 19:23:15 +0600

    done

--- Lab Final/Problem 5/X.java
+++ Lab Final/Problem 5/X.java
@@ -1,10 +1,14 @@
 class Employee{
     double salary = 30000;
-    int count;
+    static int count = 0;
+    void count_n(int n){
+        count = count + n;
+    }
 }
 
 class Teacher{
     double bonus, extra_income;
+    static int i=0;
     Employee e = new Employee();
 
     Teacher(double b, double ei){
@@ -14,14 +18,15 @@ class Teacher{
 
     void display_total_payment(int n){
         double TeacherTotalIncome = e.salary + bonus + extra_income;
-        System.out.println("Teacher's total income is : "+TeacherTotalIncome);
-        e.count = e.count + n;
-        System.out.println("e count"+e.count);
+        i = i+1;
+        System.out.println("Teacher "+i+" total income is : "+TeacherTotalIncome);
+        e.count_n(n);
     }
 }
 
 class TA{
     double bonus1;
+    static int i=0;
     Employee e = new Employee();
 
     TA(double b){
@@ -30,9 +35,9 @@ class TA{
 
     void display_total_payment1(int n){
         double TATotalIncome = e.salary + bonus1;
-        System.out.println("TA Total income is : "+ TATotalIncome);
-        e.count = e.count + n;
-        
+        i = i+1;
+        System.out.println("TA "+i+" Total income is : "+ TATotalIncome);
+        e.count_n(n);
     }
 }
 
@@ -42,9 +47,20 @@ public class X {
 
         Teacher t1 = new Teacher(100, 200);
         t1.display_total_payment(1);
-        Teacher t2 = new Teacher(100, 200);
+        Teacher t2 = new Teacher(200, 200);
         t2.display_total_payment(1);
+        Teacher t3 = new Teacher(300, 200);
+        t3.display_total_payment(1);
 
-        System.out.println("Count: " + (e.count));
+        TA ta_1 = new TA(200);
+        ta_1.display_total_payment1(1);
+        TA ta_2 = new TA(300);
+        ta_2.display_total_payment1(1);
+        TA ta_3 = new TA(400);
+        ta_3.display_total_payment1(1);
+
+
+
+        System.out.println("Total Employee: " + (e.count));
     }
 }
